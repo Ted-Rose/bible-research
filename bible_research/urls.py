67fd42e0597a552ef03b19da50ba8fb2e25dd293
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2025-06-17 20:08:43 +0300

    Adjust `urls.py`

--- bible_research/urls.py
+++ bible_research/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # This line will make your API endpoints available under /api/v1/
     path('api/v1/', include('bible.urls')),
     # This is useful for logging into the browsable API if you use DRF's authentication
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
