42588908785ba86767e442233c4c9fe2b383a7a8
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2025-06-21 18:13:10 +0300

    notes-endpoint

--- bible/models.py
+++ bible/models.py
@@ -3,16 +3,16 @@ from django.db import models
 
 
 class Verse(models.Model):
-    id = models.CharField(max_length=15, primary_key=True)
+    id = models.CharField(
+        max_length=18,
+        default=f"VER{str(uuid.uuid4()).upper().replace('-', '')[:15]}",
+        primary_key=True,
+        editable=False,
+        help_text="Unique identifier for the tag."
+    )
     book = models.CharField(max_length=50)
     chapter = models.IntegerField()
     verse = models.IntegerField()
-
-    def save(self, *args, **kwargs):
-        if not self.id:
-            uuid_str = str(uuid.uuid4()).upper().replace('-', '')
-            self.id = f"VER{uuid_str[:12]}"
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.book} {self.chapter}:{self.verse}'
