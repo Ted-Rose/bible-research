9fe85dcc02ea17a363e9090fdd744afd6ffc47c8
Ted-Rose <75977150+Ted-Rose@users.noreply.github.com>
2025-06-21 08:21:05 +0300

    notes-endpoint

--- bible/models.py
+++ bible/models.py
@@ -1,18 +1,18 @@
-import uuid
 from django.db import models
+from bible_research.utils import generate_id
 
 
 class Verse(models.Model):
-    id = models.CharField(max_length=15, primary_key=True)
+    id = models.CharField(
+        max_length=18,
+        default=generate_id('VER'),
+        primary_key=True,
+        editable=False,
+        help_text="Unique identifier for the tag."
+    )
     book = models.CharField(max_length=50)
     chapter = models.IntegerField()
     verse = models.IntegerField()
-
-    def save(self, *args, **kwargs):
-        if not self.id:
-            uuid_str = str(uuid.uuid4()).upper().replace('-', '')
-            self.id = f"VER{uuid_str[:12]}"
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.book} {self.chapter}:{self.verse}'
--- bible_research/settings.py
+++ bible_research/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
     # Third-party apps
     'rest_framework',
+    'corsheaders',
 
     # My apps
     'bible.apps.BibleConfig',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://reactive-bible.vercel.app",
 ]
 
 ROOT_URLCONF = 'bible_research.urls'
