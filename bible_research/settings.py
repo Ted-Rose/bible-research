26953e22264da6e70668b5fa628922b34e22dc7c
Ted-Rose <75977150+Ted-Rose@users.noreply.github.com>
2025-06-28 09:08:43 +0300

    Revert "Add `CORS_ALLOW_HEADERS` and `CORS_ORIGIN_WHITELIST`"
    This reverts commit 2b856f138a1184abe8d88ac403e4274965ebf814.

--- bible_research/settings.py
+++ bible_research/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import yaml
-from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -68,18 +67,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://*.vercel.app",
 ]
-
-CORS_ALLOW_HEADERS = default_headers + (
-    "Content-Type",
-    "Authorization",
-    "Accept",
-)
-
-CORS_ORIGIN_WHITELIST = (
-    "http://localhost:8000",
-    "http://localhost:5173/",
-    "https://*.vercel.app",
-    )
 
 ROOT_URLCONF = 'bible_research.urls'
 
