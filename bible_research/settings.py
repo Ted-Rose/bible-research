b4e1be1f849a84a9a23b3c340360f7cbce2ea7b5
Ted-Rose <75977150+Ted-Rose@users.noreply.github.com>
2025-06-28 09:18:47 +0300

    Keep reactive-bible.vercel.app in allowed hosts list

--- bible_research/settings.py
+++ bible_research/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     'bible-research.vercel.app',
+    'reactive-bible.vercel.app',
     'bible-research-imdtwlgzd-industr-analysers-projects.vercel.app',
 ]
 
