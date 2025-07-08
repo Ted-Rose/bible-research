e4b71e7fd25515ee245d11a42227adb9a665df0e
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2025-07-08 12:28:13 +0300

    Allow all Vercel automatically deployed PR preview origins

--- bible_research/settings.py
+++ bible_research/settings.py
@@ -66,8 +66,11 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://reactive-bible.vercel.app",
-    'https://reactive-bible-4n3yzn5f6-vincent-uches-projects.vercel.app',
-    'https://reactive-bible-rmco0i8uv-vincent-uches-projects.vercel.app',
+]
+
+# Allow all Vercel automatically deployed PR preview origins
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*-vincent-uches-projects\.vercel\.app$",
 ]
 
 ROOT_URLCONF = 'bible_research.urls'
